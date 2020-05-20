aa28bcdda5e0f420a98050e6e672fca1e6c15d2a
Andrew Venson <vensoneandrew@gmail.com>
2020-05-20 18:16:58 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+  const gridContainer = {
+    display: "grid",
+    gridTemplateColumns: "40% 60%",
+    height: "90vh",
+  };
   return (
-    <div className="App">
-      <h1>Weathercast</h1>
-    </div>
+    <>
+      <Header />
+      <div style={gridContainer}>
+        <div
+          style={{
+            border: "1px solid lightgray",
+            backgroundColor: "whitesmoke",
+          }}
+        >
+          <h1>Weathercast</h1>
+        </div>
+        <div style={{ border: "1px solid lightgray" }}></div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "gray",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <h3>Weathercast</h3>
+    </div>
+  );
+};
+
+export default Header;
